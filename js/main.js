d97f755bf0b567aac3007e5bfb8356e6ae3471c1
Abdullokh1 <giyasovabdullo0@gmail.com>
2022-02-06 16:38:51 +0500

    faq added

--- js/main.js
+++ js/main.js
@@ -8,6 +8,57 @@ let elclose = document.getElementById('closeBtn');
 
 let elheadLogo = document.getElementById('headerLogo');
 
+let elopenanswer = document.getElementById('faqBtn');
+
+let elshow = document.getElementById('showAnswer');
+
+let elopenanswertwo = document.getElementById('faqBtn-2');
+
+let elshowtwo = document.getElementById('showAnswer-2');
+
+let elopenanswerthree = document.getElementById('faqBtn-3');
+
+let elshowthree = document.getElementById('showAnswer-3');
+
+let elopenanswerfour = document.getElementById('faqBtn-4');
+
+let elshowfour = document.getElementById('showAnswer-4');
+
+elopenanswerfour.addEventListener('click', function(){
+  elshowfour.classList.toggle('answer');
+})
+
+elopenanswerfour.addEventListener('click', function(){
+  elopenanswerfour.classList.toggle('change-img');
+})
+
+
+elopenanswerthree.addEventListener('click', function(){
+  elshowthree.classList.toggle('answer');
+})
+
+elopenanswerthree.addEventListener('click', function(){
+  elopenanswerthree.classList.toggle('change-img');
+})
+
+
+elopenanswertwo.addEventListener('click', function(){
+  elshowtwo.classList.toggle('answer');
+})
+
+elopenanswertwo.addEventListener('click', function(){
+  elopenanswertwo.classList.toggle('change-img');
+})
+
+
+elopenanswer.addEventListener('click', function(){
+  elshow.classList.toggle('answer');
+})
+
+elopenanswer.addEventListener('click', function(){
+  elopenanswer.classList.toggle('change-img');
+})
+
 eldark.addEventListener('click', function(){
   document.body.classList.toggle('dark');
 })
